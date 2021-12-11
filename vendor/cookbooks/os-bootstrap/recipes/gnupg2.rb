c121c954370d218c571771e121d11485f5e5938f
Roy Liu <carsomyr@gmail.com>
2021-12-11 00:24:10 -0500

    Use the `Os::Bootstrap::Homebrew` helper library in the `gnupg2` recipe to get the Homebrew-managed executable path

--- vendor/cookbooks/os-bootstrap/recipes/gnupg2.rb
+++ vendor/cookbooks/os-bootstrap/recipes/gnupg2.rb
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright 2014 Roy Liu
+# frozen_string_literal: true
+
+# Copyright 2014-2021 Roy Liu
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -16,38 +16,39 @@
 
 class << self
   include Os::Bootstrap
+  include Os::Bootstrap::Homebrew
 end
 
 include_recipe "os-bootstrap::homebrew"
 
 recipe = self
-script_file = (owner_dir + ".profile.d/0001_gnupg2.sh")
+script_file = owner_dir.join(".profile.d/0001_gnupg2.sh")
 
 package "gnupg2" do
   action :install
 end
 
 directory "create the `~/Library` directory for #{recipe_full_name}" do
-  path (recipe.owner_dir + "Library").to_s
+  path recipe.owner_dir.join("Library").to_s
   owner recipe.owner
   group recipe.owner_group
-  mode 0700
+  mode 0o700
   action :create
 end
 
 directory "create the `~/Library/LaunchAgents` directory for #{recipe_full_name}" do
-  path (recipe.owner_dir + "Library/LaunchAgents").to_s
+  path recipe.owner_dir.join("Library/LaunchAgents").to_s
   owner recipe.owner
   group recipe.owner_group
-  mode 0755
+  mode 0o755
   action :create
 end
 
 directory "create `.profile.d` for #{recipe_full_name}" do
-  path (recipe.owner_dir + ".profile.d").to_s
+  path recipe.owner_dir.join(".profile.d").to_s
   owner recipe.owner
   group recipe.owner_group
-  mode 0755
+  mode 0o755
   action :create
 end
 
@@ -56,16 +57,17 @@ template script_file.to_s do
   source "bash-0001_gnupg2.sh.erb"
   owner recipe.owner
   group recipe.owner_group
-  mode 0755
+  mode 0o755
+  helper(:prefix) { recipe.homebrew_prefix }
   action :create
 end
 
 # Install the user agent plist.
-template (owner_dir + "Library/LaunchAgents/homebrew.gnupg2.gpg-agent.plist").to_s do
+template owner_dir.join("Library/LaunchAgents/homebrew.gnupg2.gpg-agent.plist").to_s do
   source "gnupg2-homebrew.gnupg2.gpg-agent.plist.erb"
   owner recipe.owner
   group recipe.owner_group
-  mode 0644
+  mode 0o644
   helper(:script_file) { script_file }
   notifies :restart, "service[homebrew.gnupg2.gpg-agent]", :immediately
   action :create
@@ -75,25 +77,25 @@ service "homebrew.gnupg2.gpg-agent" do
   action :nothing
 end
 
-directory (owner_dir + ".gnupg").to_s do
+directory owner_dir.join(".gnupg").to_s do
   owner recipe.owner
   group recipe.owner_group
-  mode 0700
+  mode 0o700
   action :create
 end
 
-template (owner_dir + ".gnupg/gpg.conf").to_s do
+template owner_dir.join(".gnupg/gpg.conf").to_s do
   source "gnupg2-gpg.conf.erb"
   owner recipe.owner
   group recipe.owner_group
-  mode 0600
+  mode 0o600
   action :create
 end
 
-template (owner_dir + ".gnupg/gpg-agent.conf").to_s do
+template owner_dir.join(".gnupg/gpg-agent.conf").to_s do
   source "gnupg2-gpg-agent.conf.erb"
   owner recipe.owner
   group recipe.owner_group
-  mode 0600
+  mode 0o600
   action :create
 end
