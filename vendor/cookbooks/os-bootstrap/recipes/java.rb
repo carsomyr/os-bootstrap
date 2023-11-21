339b6c717aaf522746d071cb17addccbdba39e31
Roy Liu <carsomyr@gmail.com>
2023-11-21 16:56:17 -0500

    Add the `java` recipe

---
+++ vendor/cookbooks/os-bootstrap/recipes/java.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+#
+# Copyright 2023 Roy Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+package "openjdk" do
+  action :install
+end
+
+link "/Library/Java/JavaVirtualMachines/openjdk.jdk" do
+  to "/opt/homebrew/opt/openjdk/libexec/openjdk.jdk"
+  owner "root"
+  group "wheel"
+  action :create
+end
